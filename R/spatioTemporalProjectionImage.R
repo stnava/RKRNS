cbf4ec382379390d6eaf60793ec69f07e15dc295
BBAvants <stnava@gmail.com>
2014-07-05 21:07:33 -0400

    ENH: spacetime summary

---
+++ R/spatioTemporalProjectionImage.R
@@ -0,0 +1,18 @@
+spatioTemporalProjectionImage <- function( spacetimevec, timelength, summaryFUN=mean, nDminus1ref=NA ) {
+  mm<-matrix(spacetimevec,nrow=timelength)
+  myestimatedhrf<-rowMeans(mm)
+  mmmag<-sqrt( mm^2 )
+  myestimatedbrainregionsval<-apply(mmmag,FUN=summaryFUN,MARGIN=2)
+  spaceimage<-NA
+  if ( !is.na(nDminus1ref) )
+    {
+    spaceimage<-antsImageClone( nDminus1ref )
+    indexvec<-spaceimage > 0
+    if ( sum(indexvec) == length( myestimatedbrainregionsval) )
+      {
+      spaceimage[ indexvec ]<-0
+      spaceimage[ nDminus1ref > 0 ]<-myestimatedbrainregionsval
+      }
+  }
+  return( list(spacefunction=myestimatedbrainregionsval, timefunction=myestimatedhrf, spaceimage=spaceimage ) )
+} 
