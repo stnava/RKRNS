e2287d0eaaaa8b18026ac6d2d51cdacba510ba3c
BBAvants <stnava@gmail.com>
2014-07-25 14:23:36 -0400

    WIP: glm denoise in R

--- R/glmDenoiseR.R
+++ R/glmDenoiseR.R
@@ -1,5 +1,6 @@
-glmDenoiseR <- function( boldmatrix, designmatrix , hrfbasislength=50, kfolds=10, whichbase = NA, pvalthresh=0.25, maxnoisepreds=6 )
+glmDenoiseR <- function( boldmatrix, designmatrixIn , hrfbasislength=50, kfolds=4, whichbase = NA, pvalthresh=0.25, maxnoisepreds=12 )
 {
+designmatrix<-designmatrixIn[,colMeans(designmatrixIn)>0 ]
 mygamma <- function(x, a1 = 6.,   a2 = 12., b1 = 0.9, b2 = 0.99, cc = 0.35) {
     d1 <- a1 * b1
     d2 <- a2 * b2
@@ -29,6 +30,7 @@ for ( i in 1:ncol(designmatrixext) )
   if ( k == 0 ) k<-nbase
   designmatrixext[,i]<-conv( designmatrixext[,i]  , basismat[,k] )[1:nrow(designmatrixext)]
   }
+# overall description of the method
 # 1. regressors include - design + trends + noise-pool
 # 2. need to find noise-pool by looking at high p-value voxels
 # 3. cross-validate predictions using different numbers of noise regressors
@@ -41,14 +43,36 @@ for ( i in 1:ncol(designmatrixext) )
 p<-stats::poly( 1:nrow(designmatrixext) ,degree=4)
 polyplusdesign<-cbind(designmatrixext,p)
 mylm<-lm( data.matrix(boldmatrix)  ~  polyplusdesign )
-noisevox<-biglm$pval.model > pvalthresh
+biglm1<-bigLMStats( mylm, 0.01 )
+noisevox<-biglm1$pval.model > pvalthresh
 noiseu<-svd( data.matrix(boldmatrix)[,noisevox], nv=0, nu=maxnoisepreds )$u
-mylmlist<-list()
+groups<-c()
+grouplength<-round(nrow(boldmatrix)/10)-1
+for ( k in 1:kfolds ) groups<-c(groups,rep(k,grouplength))
+bestnoiselev<-rep(0,maxnoisepreds)
 for ( i in 1:maxnoisepreds )
   {
-  mylm<-lm( data.matrix(boldmatrix)  ~  polyplusdesign + noiseu[,1:i] )
-  mylmlist<-lappend( mylmlist, mylm )
-  }  
+  noisepredcorrs<-rep(0,kfolds)
+  for ( k in 1:kfolds )
+    {
+    selector <- groups!=k
+    mydf<-data.frame( polyplusdesign[selector,] , noiseu[selector,1:i] )
+    mylm1<-lm( data.matrix( boldmatrix[selector,] )  ~  . , data=mydf )
+    selector <- groups==k
+    mydf<-data.frame( polyplusdesign[selector,] , noiseu[selector,1:i] )
+    predmat<-predict(mylm1,newdata=mydf)
+    realmat<-boldmatrix[selector,]
+    abscor<-0
+    for ( v in 1:ncol(predmat) ) abscor<-abscor+abs( cor( predmat[,v], realmat[,v] ) )
+    abscor<-abscor/ncol(predmat)
+    noisepredcorrs[k]<-abscor
+    }
+  print(paste("NoiseU:",i,"MeanCorr",mean(noisepredcorrs)))
+  bestnoiselev[i]<-mean(noisepredcorrs)
+  }
+print(bestnoiselev)
+mylm<-lm( data.matrix(boldmatrix)  ~  polyplusdesign + noiseu[selector,1:which.min(bestnoiselev)]  )
+biglm2<-bigLMStats( mylm, 0.01 )
+return( list( biglm1=biglm1, biglm2=biglm2 ) )
 # v<-residuals(m)+coefficients(m)[1]
-return(mylm)
 }
