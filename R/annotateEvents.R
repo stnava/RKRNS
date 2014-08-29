58c810195f8be81477b3ad40329a5826abb91efd
BBAvants <stnava@gmail.com>
2014-08-29 15:14:49 -0400

    BUG: removed annotator

--- R/annotateEvents.R
+++ R/annotateEvents.R
@@ -12,9 +12,9 @@ annotateEvents <- function( sentencesIn, words, eventtimes, eventnames )
   enouns2<-rep(NA,nrow(eventdata))
   enouns2lab<-rep(NA,nrow(eventdata))
   sentlab<-rep(NA,nrow(eventdata))
-  sent_token_annotator <- Maxent_Sent_Token_Annotator()
-  word_token_annotator <- Maxent_Word_Token_Annotator()
-  pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+#  sent_token_annotator <- Maxent_Sent_Token_Annotator()
+#  word_token_annotator <- Maxent_Word_Token_Annotator()
+#  pos_tag_annotator <- Maxent_POS_Tag_Annotator()
   for ( i in 1:nrow(eventdata) ) {
     s<-eventdata$sentences[i]
     s<-gsub('[.]',' ',s)
@@ -40,32 +40,8 @@ annotateEvents <- function( sentencesIn, words, eventtimes, eventnames )
       ct<-ct+1
     }
     s<-paste(locwords,collapse=' ')
-    a2 <- annotate(s, list(sent_token_annotator, word_token_annotator))
-    a3 <- annotate(s, pos_tag_annotator, a2)
-    a3w <- subset(a3, type == "word")
-    tags <- sapply(a3w$features, `[[`, "POS")
-#    grepverb<-grep("V",tags[locwordpos])
-    grepverb<-grep("V",tags)
-    if (length(grepverb)>0){
-    everbs1[i]<-locwords[grepverb]
-    rpl<-as.character(worddf$words) == everbs1[i]
-    if ( sum(rpl) > 0 ) everbs1lab[i]<-worddf$wordids[ rpl ]
-    }
-#    grepnouns<-grep("NN",tags[locwordpos])
-    grepnouns<-grep("NN",tags)
-    enouns1[i]<-locwords[grepnouns[1]]
-    if ( !is.na(enouns1[i]) )
-      {
-      rpl<-as.character(worddf$words) == enouns1[i]
-      if ( sum(rpl) > 0 ) enouns1lab[i]<-worddf$wordids[ rpl ]
-      nextnoun<-grepnouns[1]
-      if ( length(grepnouns) > 1 ) nextnoun<-grepnouns[2]
-      enouns2[i]<-locwords[nextnoun]
-      rpl<-as.character(worddf$words) == enouns2[i]
-      if ( sum(rpl) > 0 ) enouns2lab[i]<-worddf$wordids[ rpl ]
-      }
   }
-eventdata<-cbind( eventdata, enouns1=enouns1, enouns1lab=enouns1lab, everbs1=everbs1, everbs1lab=everbs1lab, enouns2=enouns2, enouns2lab=enouns2lab, sentlab=sentlab )
+eventdata<-cbind( eventdata, sentlab=sentlab )
 
 return( eventdata )
   
